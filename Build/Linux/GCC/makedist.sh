e3c548716fcb8e20873e6673267e625f2e073f3d
Francesco Gazzetta <fgaz@fgaz.me>
2019-03-20 15:39:01 +0100

    Move usr/ in the cmake prefix
    Usually the prefix is set to /usr or /usr/local, so adding usr/ again to
    the prefix path is redundant. Also some distributions (GoboLinux, NixOS,
    GuixSD) do not use /usr as prefix.

--- Build/Linux/GCC/makedist.sh
+++ Build/Linux/GCC/makedist.sh
@@ -5,7 +5,7 @@ BUILD_DIR="$PROJ_ROOT/build"
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-cmake -DCMAKE_MODULE_PATH=$PROJ_ROOT -DCMAKE_INSTALL_PREFIX=$PROJ_ROOT/dist $PROJ_ROOT
+cmake -DCMAKE_MODULE_PATH=$PROJ_ROOT -DCMAKE_INSTALL_PREFIX=$PROJ_ROOT/dist/usr $PROJ_ROOT
 #/Build/Linux/GCC
 make && make install/strip
 find $PROJ_ROOT/dist/ -type d | xargs chmod 755
